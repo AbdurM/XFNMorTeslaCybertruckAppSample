510baff6a2ab42e312dfdbfafa03e67c61596c69
abdur <abdur.mohammad@xam-consulting.com>
2020-08-04 00:05:24 +1000

    initial commit

---
+++ TeslaCybertruckAppSample/Views/CircularButtonControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.NeoControls;
+
+namespace TeslaCybertruckAppSample.Views
+{
+    public partial class CircularButtonControl : NeoFrame
+    {
+        public CircularButtonControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TeslaCybertruckAppSample/Views/RectangularSwitch.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.NeoControls;
+
+namespace TeslaCybertruckAppSample.Views
+{
+    public partial class RectangularSwitch : NeoFrame
+    {
+        public RectangularSwitch()
+        {
+            InitializeComponent();
+        }
+    }
+}
